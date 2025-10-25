660fd57054832a52786e997b64eda17c8ed2f40c
LeeluGit <vleelendrakumar@gmail.com>
2025-10-25 16:07:59 +0530

    Java Basic Programs

---
+++ Java_Programs/MathOperations.java
@@ -0,0 +1,12 @@
+public class MathOperations {
+
+    // Method that takes two integers and returns their sum
+    public static int findMaximum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = findMaximum(10, 20);
+        System.out.println("Sum of the two numbers: " + result);
+    }
+}
